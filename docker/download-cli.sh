75d2cffef90eb792b3f47dacb4f9793c047c59af
Enrico.Schuetz <enrico.schuetz@evosoft.com>
2024-12-16 14:17:30 +0100

    initial

--- docker/download-cli.sh
+++ docker/download-cli.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-URL=$(curl -s https://api.github.com/repos/wot-oss/tmc/releases/latest | jq -r '.assets | .[] | select(.name == "tmc-linux-amd64") | .browser_download_url')
+URL=$(curl -s https://api.github.com/repos/Jokers13/tmc/releases/latest | jq -r '.assets | .[] | select(.name == "tmc-linux-amd64") | .browser_download_url')
 
 curl -OL $URL
 mv ./tmc-linux-amd64 ./tmc
